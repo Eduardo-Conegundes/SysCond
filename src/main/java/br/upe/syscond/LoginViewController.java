942c3e8ffac11102fd3e764a72e1d16d57e8877b
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2021-05-08 16:12:36 -0300

    Main melhorada e adicionado Material Icons na POM

--- src/main/java/br/upe/syscond/LoginViewController.java
+++ src/main/java/br/upe/syscond/LoginViewController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+public class LoginViewController {
 	static InterfaceUsuarioController userCont = new UsuarioController();
 	
     @FXML
